b41b2ef92cf7985f883452007b746a1ee37ab595
Rishu kumar <117526503+ma018nc@users.noreply.github.com>
2025-12-28 13:12:24 +0530

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-from backend.app.main import app
-
-if __name__ == "__main__":
-    # Hugging Face Spaces expects apps to listen on port 7860
-    uvicorn.run(app, host="0.0.0.0", port=7860)
